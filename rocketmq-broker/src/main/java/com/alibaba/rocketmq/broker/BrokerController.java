325200c9db018e61598d5e894ead7b317d3cdbfc
Li Zhanhui <lizhanhui@gmail.com>
2016-08-31 18:58:16 +0800

    Typo fix

--- rocketmq-broker/src/main/java/com/alibaba/rocketmq/broker/BrokerController.java
+++ rocketmq-broker/src/main/java/com/alibaba/rocketmq/broker/BrokerController.java
@@ -133,7 +133,7 @@ public class BrokerController {
 
         if (this.brokerConfig.getNamesrvAddr() != null) {
             this.brokerOuterAPI.updateNameServerAddressList(this.brokerConfig.getNamesrvAddr());
-            log.info("user specfied name server address: {}", this.brokerConfig.getNamesrvAddr());
+            log.info("user specified name server address: {}", this.brokerConfig.getNamesrvAddr());
         }
 
         this.slaveSynchronize = new SlaveSynchronize(this);
