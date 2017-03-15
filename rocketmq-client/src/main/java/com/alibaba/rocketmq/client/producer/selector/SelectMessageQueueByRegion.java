f557c921af297273519e9265e4052358983eef66
Li Zhanhui <lizhanhui@apache.org>
2017-03-15 21:25:20 +0800

    Revert message queue selector.

--- rocketmq-client/src/main/java/com/alibaba/rocketmq/client/producer/selector/SelectMessageQueueByRegion.java
+++ rocketmq-client/src/main/java/com/alibaba/rocketmq/client/producer/selector/SelectMessageQueueByRegion.java
@@ -6,19 +6,13 @@ import com.alibaba.rocketmq.common.message.MessageQueue;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class SelectMessageQueueByRegion implements MessageQueueSelector {
 
     private Region target;
 
-    private final AtomicReference<List<MessageQueue>> targetMessageQueues = new AtomicReference<>();
-
-    private final AtomicReference<List<MessageQueue>> messageQueues = new AtomicReference<>();
-
-    private final AtomicLong cursor = new AtomicLong();
+    private ConcurrentHashMap<MessageQueue, Integer> messageQueueRegionMap = new ConcurrentHashMap<>();
 
     public SelectMessageQueueByRegion(Region target) {
         if (null == target) {
@@ -26,63 +20,35 @@ public class SelectMessageQueueByRegion implements MessageQueueSelector {
         } else {
             this.target = target;
         }
-
-        if (Region.SAME == this.target) {
-            this.target = Region.parse(Integer.parseInt(Util.LOCAL_DATA_CENTER_ID));
-        }
-
-        schedule();
-    }
-
-    private void schedule() {
-        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor(new ThreadFactory() {
-            @Override
-            public Thread newThread(Runnable runnable) {
-                Thread thread = new Thread(runnable, "SelectMessageQueueByRegion");
-                thread.setDaemon(true);
-                return thread;
-            }
-        });
-
-        scheduler.scheduleWithFixedDelay(new Runnable() {
-            @Override
-            public void run() {
-                update();
-            }
-        }, 30, 30, TimeUnit.SECONDS);
-    }
-
-    private void update() {
-        if (Region.ANY == this.target) {
-            targetMessageQueues.set(messageQueues.get());
-            return;
-        }
-
-        List<MessageQueue> all = messageQueues.get();
-        List<MessageQueue> result = new ArrayList<>();
-        for (MessageQueue messageQueue : all) {
-            if (Util.getRegionIndex(messageQueue) == target.getIndex()) {
-                result.add(messageQueue);
-            }
-        }
-
-        if (!result.isEmpty()) {
-            targetMessageQueues.set(result);
-        } else {
-            targetMessageQueues.set(all);
-        }
     }
 
     @Override
     public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
-        messageQueues.set(mqs);
 
-        if (null == targetMessageQueues.get()) {
-            update();
+        if (Region.SAME == target) {
+            target = Region.parse(Integer.parseInt(Util.LOCAL_DATA_CENTER_ID));
         }
 
-        List<MessageQueue> messageQueues = targetMessageQueues.get();
+        if (Region.ANY == target) {
+            return Util.random(mqs);
+        }
 
-        return messageQueues.get((int)(cursor.getAndIncrement() % messageQueues.size()));
+        List<MessageQueue> list = new ArrayList<>();
+
+        for (MessageQueue messageQueue : mqs) {
+            if (!messageQueueRegionMap.containsKey(messageQueue)) {
+                messageQueueRegionMap.put(messageQueue, Util.getRegionIndex(messageQueue));
+            }
+
+            if (messageQueueRegionMap.get(messageQueue) == target.getIndex()) {
+                list.add(messageQueue);
+            }
+        }
+
+        if (list.isEmpty()) {
+            return Util.random(mqs);
+        }
+
+        return Util.random(list);
     }
 }
